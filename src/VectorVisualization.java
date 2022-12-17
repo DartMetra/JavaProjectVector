02e1073a95b9ad96cfbc97d35b1b234903a8c61b
mr10us <minenkoduck@gmail.com>
2022-12-17 18:25:10 +0200

    vectorBox

--- src/VectorVisualization.java
+++ src/VectorVisualization.java
@@ -1,3 +1,5 @@
+import components.*;
+
 import java.awt.*;
 import javax.swing.*;
 import java.util.Vector;
@@ -5,4 +7,27 @@ import java.util.Vector;
 
 public class VectorVisualization extends JFrame {
     private Vector vector;
+
+    public VectorVisualization(){
+        super();
+
+        JPanel gVectors = new JPanel();
+        gVectors.setLayout(new FlowLayout(FlowLayout.LEFT,1,20));
+        gVectors.add(new VectorBox());
+        gVectors.add(new VectorBox());
+
+
+
+        this.setTitle("Вектор GUI");
+        this.setPreferredSize(new Dimension(1200, 1000));
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLayout(new GridLayout(1,2,20,20));
+
+        this.add(gVectors);
+        this.add(gVectors);
+
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
