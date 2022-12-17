80d30d1f4bad775bf944a229b136d44e9fc6437d
Maksym Kotelva <maksymkotelva@gmail.com>
2022-12-17 16:08:01 +0200

    idea

--- src/VectorVisualization.java
+++ src/VectorVisualization.java
@@ -5,19 +5,4 @@ import java.util.Vector;
 
 public class VectorVisualization extends JFrame {
     private Vector vector;
-
-    public VectorVisualization() {
-        super();
-
-        InitFrame initFrame = new InitFrame();
-
-
-        this.setTitle("Візуалізація вектора");
-        this.setMinimumSize(new Dimension(400, 700));
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.setLayout(new BorderLayout());
-        this.pack();
-        this.setLocationRelativeTo(null);
-
-    }
 }
