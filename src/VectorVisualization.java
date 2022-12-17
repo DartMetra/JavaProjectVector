f1d3a15503aaac80be6edf0255d707a9bce809e7
mr10us <minenkoduck@gmail.com>
2022-12-17 23:47:32 +0200

    govnishe

--- src/VectorVisualization.java
+++ src/VectorVisualization.java
@@ -8,23 +8,67 @@ import java.util.Vector;
 public class VectorVisualization extends JFrame {
     private Vector vector;
 
-    public VectorVisualization(){
+    public VectorVisualization() {
         super();
 
         JPanel gVectors = new JPanel();
-        gVectors.setLayout(new FlowLayout(FlowLayout.LEFT,1,20));
-        gVectors.add(new VectorBox());
-        gVectors.add(new VectorBox());
+        gVectors.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        gVectors.setBackground(Color.green);
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
+        gVectors.add(new VectorBox("1"));
+        gVectors.add(new VectorBox("2"));
 
 
+        JPanel editPanel = new JPanel();
+        editPanel.setLayout(new GridLayout(0,1,0,50));
+        editPanel.add(new JButton("Додати елемент"));
+        editPanel.add(new JButton("Додати1"));
+        editPanel.add(new JButton("Додати2"));
+        editPanel.add(new JButton("Додати3"));
+        editPanel.add(new JButton("Додати4"));
+        editPanel.add(new JLabel("Розмір вектора: null"));
+        editPanel.add(new JLabel("Ємність вектора: null"));
+        editPanel.setBackground(Color.red);
 
         this.setTitle("Вектор GUI");
-        this.setPreferredSize(new Dimension(1200, 1000));
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(1,2,20,20));
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        this.setPreferredSize(new Dimension(1200, 1000));
 
-        this.add(gVectors);
-        this.add(gVectors);
+        c.ipady = 0;
+
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.insets = new Insets(60,25,0,100);
+
+        c.weighty = 1.0;
+
+        c.gridx = 0;
+        c.gridy = 0;
+
+        this.add(gVectors, c);
+
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+
+        c.gridheight = 3;
+
+
+        c.gridx = 2;
+        c.gridy = 0;
+
+        this.add(editPanel, c);
+
 
         this.pack();
         this.setLocationRelativeTo(null);
