d1503f945b1f4c26e19f6dc268eeb6190627014a
mr10us <minenkoduck@gmail.com>
2022-12-20 21:08:27 +0200

    added actionListener for buttons

--- src/VectorVisualizationFrame.java
+++ src/VectorVisualizationFrame.java
@@ -10,7 +10,8 @@ public class VectorVisualizationFrame<Type> extends JFrame implements ActionList
     private JPanel vectorPanel = new JPanel(new GridBagLayout());
     private GridPosition vectorElemPos = new GridPosition().setInsets(5);
 
-    public VectorVisualizationFrame(int initialCapacity,int capacityIncrement) {
+
+    public VectorVisualizationFrame(int initialCapacity, int capacityIncrement) {
         super();
 
         this.vector = new Vector<Type>(initialCapacity, capacityIncrement);
@@ -39,13 +40,29 @@ public class VectorVisualizationFrame<Type> extends JFrame implements ActionList
         GridPosition position = new GridPosition().setPosition(0, 0).setInsets(15, 5, 15, 5);
         JPanel controlPanel = new JPanel(new GridBagLayout());
         Btn addBtn = new Btn(HtmlHelper.plain("Додати елемент"), new ImageIcon("images/add.png"));
+        addBtn.addActionListener(this);
+        addBtn.setActionCommand("addElem");
         Btn removeBtn = new Btn(HtmlHelper.plain("Видалити елемент"), new ImageIcon("images/remove.png"));
+        removeBtn.addActionListener(this);
+        removeBtn.setActionCommand("removeElem");
         Btn insertBtn = new Btn(HtmlHelper.plain("Вставити елемент"), new ImageIcon("images/insert.png"));
+        insertBtn.addActionListener(this);
+        insertBtn.setActionCommand("insertElem");
         Btn setBtn = new Btn(HtmlHelper.plain("Замінити елемент"), new ImageIcon("images/set.png"));
+        setBtn.addActionListener(this);
+        setBtn.setActionCommand("setElem");
         Btn setSizeBtn = new Btn(HtmlHelper.plain("Встановити розмір вектора"), new ImageIcon("images/setSize.png"));
+        setSizeBtn.addActionListener(this);
+        setSizeBtn.setActionCommand("setSize");
         Btn clearBtn = new Btn(HtmlHelper.plain("Очистити вектор"), new ImageIcon("images/clear.png"));
+        clearBtn.addActionListener(this);
+        clearBtn.setActionCommand("clear");
         Btn trimToSizeBtn = new Btn(HtmlHelper.plain("Обрізати до розміру"), new ImageIcon("images/trimToSize.png"));
-
+        trimToSizeBtn.addActionListener(this);
+        trimToSizeBtn.setActionCommand("trim");
+        Btn removeRangeBtn = new Btn(HtmlHelper.plain("Видалити ряд елементів"), new ImageIcon("images/removeRange.png"));
+        removeRangeBtn.addActionListener(this);
+        removeRangeBtn.setActionCommand("removeRange");
 
 
         position.fill = GridPosition.BOTH;
@@ -57,6 +74,7 @@ public class VectorVisualizationFrame<Type> extends JFrame implements ActionList
         controlPanel.add(setSizeBtn, position.nextRow());
         controlPanel.add(clearBtn, position.nextRow());
         controlPanel.add(trimToSizeBtn, position.nextRow());
+        controlPanel.add(removeRangeBtn, position.nextRow());
 
         vectorPanel.setBackground(Color.GRAY);
         this.renderVector();
@@ -82,5 +100,42 @@ public class VectorVisualizationFrame<Type> extends JFrame implements ActionList
             vectorPanel.add(new VectorElem("."));
     }
 
-    public void actionPerformed(ActionEvent event) {}
+    public void actionPerformed(ActionEvent event) {
+        String cmd = event.getActionCommand();
+
+        switch (cmd) {
+
+            case "addElem" -> {
+                System.out.println("add");
+            }
+            case "removeElem" -> {
+                System.out.println("rem");
+                /// remove an element from a vector
+            }
+            case "insertElem" -> {
+                System.out.println("insert");
+                /// inserts an element at some place into a vector
+            }
+            case "setElem" -> {
+                System.out.println("replace");
+                /// replacing an element in a vector
+            }
+            case "setSize" -> {
+                System.out.println("set");
+                /// setting the size of a vector
+            }
+            case "clear" -> {
+                System.out.println("clear");
+                /// clears a vector
+            }
+            case "trimToSize" -> {
+                System.out.println("trim");
+                /// trims to size a vector
+            }
+            case "removeRange" -> {
+                System.out.println("removeRange");
+                /// removes a range of the elements of a vector
+            }
+        }
+    }
 }
